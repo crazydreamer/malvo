273b0d74850c381a892296873cf239eeb20d9f53
Mayank Tripathi <oquanox@gmail.com>
2015-04-07 06:26:12 +0530

    Added language preference for team registration

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,11 +1,16 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, PasswordField, IntegerField, SubmitField
+from wtforms import StringField, PasswordField, IntegerField, SubmitField, \
+    RadioField
 from wtforms.validators import DataRequired, Optional
 
 
 class RegistrationForm(Form):
     name = StringField('Team Name', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
+    language = RadioField(
+        'Preferred Language', choices=[('c', 'C'), ('java', 'Java')],
+        validators=[DataRequired()]
+    )
     member_one_name = StringField('Name', validators=[DataRequired()])
     member_one_phone = IntegerField('Phone Number', validators=[DataRequired()])
     member_one_id = StringField('College ID', validators=[Optional()])
--- app/mcq/forms.py
+++ app/mcq/forms.py
@@ -1,8 +1,6 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, PasswordField, IntegerField, RadioField, SubmitField
-from wtforms.validators import DataRequired, Optional
+from wtforms import SubmitField
 
 
 class LanguageChoice(Form):
-    language = RadioField('Language Choice', choices=[('c', 'C Programming Language'), ('java', 'Java')], validators=[DataRequired()])
     submit = SubmitField('Start')
